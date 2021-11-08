a6376a935b0acbb74eeb743808bba4600b7feb22
Adrian Lara <aperez@isg.us>
2021-11-08 11:17:37 -0500

    Update dependency

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3.7 get-pip.py --upgrade --user
-pip install --upgrade pyopenssl 
-python3.7 -m pip install flask
+python3.7 get-pip.py --user
+python3.7 -m pip install --upgrade pyopenssl 
+python3.7 -m pip install --upgrade flask
