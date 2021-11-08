bfa2159e16a4e25933b7316fa20ec8608f40e28c
Adrian Lara <aperez@isg.us>
2021-11-08 11:57:59 -0500

    Add trust options

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,6 +1,7 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
+pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org pip setuptools
 pip install --upgrade requests
 pip install --upgrade pyopenssl ndg-httpsclient pyasn1
 pip install --upgrade flask
