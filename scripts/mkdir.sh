cf0e6541b38135a0c2849adbb60c1238185ec402
Adrian Lara <aperez@isg.us>
2021-11-08 11:27:43 -0500

    Fix ssl error

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py --user
-python3.7 -m pip install --upgrade pyopenssl 
+python3.7 -m pip install --upgrade requests pyopenssl ndg-httpsclient pyasn1
 python3.7 -m pip install --upgrade flask
