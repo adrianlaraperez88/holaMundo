40b9734c0e993c4013637bc075986f51a0a59f37
Adrian Lara <aperez@isg.us>
2021-11-08 12:03:27 -0500

    Add trust host in all request only for test

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -2,6 +2,5 @@ mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org pip setuptools
-pip install --upgrade requests
-pip install --upgrade pyopenssl ndg-httpsclient pyasn1
-pip install --upgrade flask
+pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade pyopenssl ndg-httpsclient pyasn1
+pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade flask
