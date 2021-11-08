efd68c197deee5875db83d85a3885f8709ed7647
Adrian Lara <aperez@isg.us>
2021-11-08 11:06:56 -0500

    Install openssl

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3.7 get-pip.py --user
-pip install --upgrade ndg-httpsclient 
-python3.7 -m pip install Flask
+python3.7 get-pip.py --upgrade --user
+pip install --upgrade pyopenssl 
+python3.7 -m pip install flask
