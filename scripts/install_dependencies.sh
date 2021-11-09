ccde7c9879c511f524224a240a11315afb570bc8
Adrian Lara <aperez@isg.us>
2021-11-09 17:08:47 -0500

    test

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-yum update -y
-yum install httpd -y
-yum install -y php
-curl -sS https://getcomposer.org/installer | php
-mv -f composer.phar /usr/bin/composer
+#Install php7
+ apt-get update
+
+ # Install PHP
+ apt-get install -y php5 php5-curl curl php5-cli git
+
+ #Install composer
+ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ php composer-setup.php ;
+ php -r "unlink('composer-setup.php');" ;
+ mv composer.phar /usr/local/bin/composer
+ composer
